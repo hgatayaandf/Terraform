b73192eebefcc2d133fd16783cdcd5c5a1f00231
hgatayaandf <dpreethy13@gmail.com>
2024-01-31 20:09:53 +0530

    updated 1 commit

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "web" {
   ami             = "ami-0c42696027a8ede58"
   instance_type   = "t2.micro"
-  key_name        = "oct3"
-  security_groups = ["linux-sg"]
+  key_name        = "cache"
+  security_groups = ["sg"]
   #  count = 2
 
   tags = {
